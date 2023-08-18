b962b18d1fc3e3e6aa790333fe2bebe42056a7dd
Andres6936 <andres6936@gmail.com>
2023-08-17 21:26:40 -0500

    Prepare Structure

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+function App() {
+  return (
+    <main className={"w-screen h-screen bg-neutral-600"}>
+        <section className={"h-full flex flex-col justify-center items-center gap-8 container mx-auto my-auto"}>
+            <article className={"flex flex-row gap-8"}>
+                <div className={"bg-white p-20"}>
+
+                </div>
+
+                <div className={"bg-white p-20"}>
+
+                </div>
+
+                <div className={"bg-white p-20"}>
+
+                </div>
+            </article>
+
+            <article className={"flex flex-row gap-8"}>
+                <div className={"bg-white p-20"}>
+
+                </div>
+
+                <div className={"bg-white p-20"}>
+
+                </div>
+
+                <div className={"bg-white p-20"}>
+
+                </div>
+            </article>
+        </section>
+    </main>
+  )
+}
+
+export default App
