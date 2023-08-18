108285e6f61221b7b8b83114a67bd42449e2de10
Andres6936 <andres6936@gmail.com>
2023-08-17 21:48:18 -0500

    Created Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,36 @@
 function App() {
     return (
         <main className={"w-screen h-screen bg-neutral-600"}>
-            <section className={"h-full flex flex-col justify-center items-center gap-8 container mx-auto my-auto"}>
-                <article className={"flex flex-row gap-8"}>
-                    <div className={"flex w-[6rem] bg-white"}>
+            <section className={"h-full flex flex-row justify-center items-center gap-8 container mx-auto my-auto"}>
+                <article className={"flex flex-col gap-8"}>
+                    <div className={"flex flex-row gap-8"}>
+                        <div className={"flex w-[6rem] bg-white"}>
+                            01
+                        </div>
+
+                        <div className={"flex w-[14rem] bg-white"}>
+                            02
+                        </div>
+                    </div>
+
+                    <div className={"flex flex-grow bg-white"}>
                         01
-                    </div>
-
-                    <div className={"flex w-[14rem] bg-white"}>
-                        02
-                    </div>
-
-                    <div className={"flex w-[26rem] bg-white"}>
-                        02
                     </div>
                 </article>
 
-                <article className={"flex flex-row gap-8"}>
-                    <div className={"flex w-[20rem] bg-white"}>
-                        01
-                    </div>
-
-                    <div className={"flex w-[6rem] bg-white"}>
+                <article className={"flex flex-col gap-8"}>
+                    <div className={"flex flex-grow bg-white"}>
                         02
                     </div>
 
-                    <div className={"flex w-[20rem] bg-white"}>
-                        03
+                    <div className={"flex flex-row gap-8"}>
+                        <div className={"flex w-[10rem] bg-white"}>
+                            02
+                        </div>
+
+                        <div className={"flex w-[16rem] bg-white"}>
+                            03
+                        </div>
                     </div>
                 </article>
             </section>
