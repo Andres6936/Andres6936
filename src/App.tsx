21b7a24dd4aa555d78365df2a5ef052dfd2aacca
Andres6936 <andres6936@gmail.com>
2023-08-17 21:42:24 -0500

    Created Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,37 @@
 function App() {
-  return (
-    <main className={"w-screen h-screen bg-neutral-600"}>
-        <section className={"h-full flex flex-col justify-center items-center gap-8 container mx-auto my-auto"}>
-            <article className={"flex flex-row gap-8"}>
-                <div className={"bg-white p-20"}>
+    return (
+        <main className={"w-screen h-screen bg-neutral-600"}>
+            <section className={"h-full flex flex-col justify-center items-center gap-8 container mx-auto my-auto"}>
+                <article className={"flex flex-row gap-8"}>
+                    <div className={"flex w-[6rem] bg-white"}>
+                        01
+                    </div>
 
-                </div>
+                    <div className={"flex w-[14rem] bg-white"}>
+                        02
+                    </div>
 
-                <div className={"bg-white p-20"}>
+                    <div className={"flex w-[26rem] bg-white"}>
+                        02
+                    </div>
+                </article>
 
-                </div>
+                <article className={"flex flex-row gap-8"}>
+                    <div className={"flex w-[20rem] bg-white"}>
+                        01
+                    </div>
 
-                <div className={"bg-white p-20"}>
+                    <div className={"flex w-[6rem] bg-white"}>
+                        02
+                    </div>
 
-                </div>
-            </article>
-
-            <article className={"flex flex-row gap-8"}>
-                <div className={"bg-white p-20"}>
-
-                </div>
-
-                <div className={"bg-white p-20"}>
-
-                </div>
-
-                <div className={"bg-white p-20"}>
-
-                </div>
-            </article>
-        </section>
-    </main>
-  )
+                    <div className={"flex w-[20rem] bg-white"}>
+                        03
+                    </div>
+                </article>
+            </section>
+        </main>
+    )
 }
 
 export default App
