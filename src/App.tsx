adfa26759263d7cac081da59fc6be03b859100e5
Andres6936 <andres6936@gmail.com>
2024-05-25 16:00:43 -0500

    Update styling and add content to div elements
    Additional padding, heading, and paragraph texts have been added to the different div sections for a better visual presentation. A new 'Lemon Template' section has also been created with specified styles and content to provide more user information.

--- src/App.tsx
+++ src/App.tsx
@@ -49,12 +49,12 @@ export default function App() {
             </section>
 
             <section className="flex flex-row gap-6 items-center justify-center w-3/4 mx-auto mt-24">
-                <div className="flex flex-[3] bg-white h-96 rounded-2xl">
-
+                <div className="flex flex-[3] bg-white h-96 rounded-2xl px-8 py-12">
+                    <h1 className="text-3xl">I've been helping</h1>
                 </div>
 
-                <div className="flex flex-[2] bg-black h-96 rounded-2xl">
-
+                <div className="flex flex-[2] bg-black h-96 rounded-2xl px-4 py-12">
+                    <h1 className="text-3xl text-white">Interface Design</h1>
                 </div>
             </section>
 
@@ -63,8 +63,23 @@ export default function App() {
 
                 </div>
 
-                <div className="flex flex-[3] bg-white h-96 rounded-2xl">
+                <div className="flex flex-col flex-[3] bg-white h-96 rounded-2xl px-4 pt-8 pb-4">
+                    <div className="flex flex-row justify-between items-center px-6">
+                        <div className="flex flex-col flex-1">
+                            <h1 className="text-3xl">Lemon Template</h1>
+                            <p className="mt-2">An template from Flowbase</p>
+                        </div>
 
+                        <div>
+                            <div className="w-12 aspect-square bg-zinc-100 rounded-full">
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-1 bg-zinc-100 mt-4 rounded-2xl">
+
+                    </div>
                 </div>
             </section>
 
