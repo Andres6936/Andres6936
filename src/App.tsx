6632b98b4c78cd1eddd004eac37434a2880a1d36
Andres6936 <andres6936@gmail.com>
2024-05-25 19:40:01 -0500

    Add new project section in App component
    A new 'project initiation' section has been added to the main App component, which includes a responsive 'Let's Talk' button. This new addition enhances the user interaction model by encouraging users to start a project.

--- src/App.tsx
+++ src/App.tsx
@@ -183,6 +183,24 @@ export default function App() {
                     Contact Me - it's free
                 </button>
             </section>
+
+            <section className="flex flex-col bg-white rounded-2xl w-3/4 mx-auto mt-24 p-8 py-16">
+                <div className="border border-zinc-300 p-2 w-3/6 mx-auto rounded-full">
+                    <h1 className="text-5xl font-semibold text-center ">Want to start</h1>
+                </div>
+                <h1 className="text-5xl font-semibold text-center w-3/6 mx-auto">a project?</h1>
+
+                <button
+                    className="flex flex-row gap-8 justify-around items-center bg-yellow-300 rounded-full px-1 py-1 w-fit mx-auto mt-12">
+                    <p className="px-4 font-semibold">Let's Talk</p>
+                    <div
+                        className="flex items-center justify-center w-12 aspect-square bg-white rounded-full shadow-lg">
+                        <div className="w-3 bg-black rounded-full aspect-square">
+
+                        </div>
+                    </div>
+                </button>
+            </section>
         </main>
     )
 }
