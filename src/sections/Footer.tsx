89c4d2141472897dfc8e2c38f86e9bee6deeb713
Andres6936 <andres6936@gmail.com>
2024-05-25 22:05:58 -0500

    Update Footer layout and style
    The Footer section's layout and style have been enhanced. Specifically, the footer width has been increased slightly, gaps have decreased and horizontal rules have been added for better visual separation in smaller (mobile) viewports. Additionally, some items alignment has been adjusted for optimal presentation across various screen sizes.

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,10 +1,13 @@
 export default function Footer() {
     return (
-        <section className="flex flex-col md:flex-row justify-between bg-white rounded-2xl w-3/4 mx-auto mt-24 p-8">
+        <section
+            className="flex flex-col md:flex-row gap-4 md:gap-0 justify-between bg-white rounded-2xl w-5/6 md:w-3/4 mx-auto mt-24 p-8">
             <div>
                 <p className="font-semibold text-zinc-400 mb-1">Work with us</p>
                 <p>andres6936@live.com</p>
             </div>
+
+            <hr className="md:hidden"/>
 
             <div>
                 <p className="font-semibold text-zinc-400 mb-1">Follow us</p>
@@ -15,7 +18,9 @@ export default function Footer() {
                 </div>
             </div>
 
-            <div className="flex flex-row gap-3 items-center">
+            <hr className="md:hidden"/>
+
+            <div className="flex flex-row gap-3 items-center justify-end md:justify-start">
                 <div className="w-10 bg-yellow-300 aspect-square rounded-full">
 
                 </div>
