1d93a1ad24faa2e93aeef4c48e2f7d372664f6da
Andres6936 <andres6936@gmail.com>
2024-05-25 22:14:04 -0500

    Update responsive behavior for text size and section width
    Adjusted the text sizing and width of multiple sections in the Contact and WantStart components to be responsive. This change ensures that on smaller screens the text and section sizes scale down appropriately for better readability and user experience.

--- src/sections/WantStart.tsx
+++ src/sections/WantStart.tsx
@@ -1,10 +1,10 @@
 export default function WantStart() {
     return (
         <section className="flex flex-col bg-white rounded-2xl w-5/6 md:w-3/4 mx-auto mt-24 p-8 py-16">
-            <div className="border border-zinc-300 p-2 w-3/6 mx-auto rounded-full">
-                <h1 className="text-5xl font-semibold text-center ">Want to start</h1>
+            <div className="border border-zinc-300 p-2 w-full md:w-3/6 mx-auto rounded-full">
+                <h1 className="text-4xl md:text-5xl font-semibold text-center">Want to start</h1>
             </div>
-            <h1 className="text-5xl font-semibold text-center w-3/6 mx-auto">a project?</h1>
+            <h1 className="text-4xl md:text-5xl font-semibold text-center w-full md:w-3/6 mx-auto">a project?</h1>
 
             <button
                 className="flex flex-row gap-8 justify-around items-center bg-yellow-300 rounded-full px-1 py-1 w-fit mx-auto mt-12">
