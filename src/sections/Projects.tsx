0ff2122ad3c8d7533a0611b3fffab4c530cde78e
Andres6936 <andres6936@gmail.com>
2024-05-28 11:10:53 -0500

    Update layout and styling in Projects section
    This commit updates the layout and styling within the Projects section of the page. The div elements were dynamically updated with new classes for improved appearance and functionality. In addition, an image was inserted in the layout, enhancing visual appeal.

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -14,12 +14,20 @@ export default function Projects() {
 
             <section
                 className="flex flex-col md:flex-row gap-6 items-center justify-center md:w-5/6 lg:w-3/4 mx-auto mt-6">
-                <div className="flex w-5/6 md:flex-[2] order-last md:order-none bg-white h-96 rounded-2xl">
+                <div
+                    className="flex flex-col overflow-hidden w-5/6 p-3 md:flex-[2] order-last md:order-none bg-white h-[36rem] 2xl:h-[48rem] rounded-2xl">
+                    <div className="flex flex-col flex-1 bg-zinc-100 rounded-2xl">
+                        <div className="flex min-h-28 h-28 bg-white rounded-b-3xl">
 
+                        </div>
+                        <div className="flex w-7/12 mx-auto my-auto">
+                            <img className="w-full rounded-2xl" src="/img/Respikar-Index.jpeg" alt="Image"/>
+                        </div>
+                    </div>
                 </div>
 
                 <div
-                    className="flex flex-col w-5/6 md:flex-[3] order-1 md:order-none bg-white h-96 rounded-2xl px-4 pt-8 pb-4">
+                    className="flex flex-col w-5/6 md:flex-[3] order-1 md:order-none bg-white h-[36rem] 2xl:h-[48rem] rounded-2xl px-4 pt-8 pb-4">
                     <div className="flex flex-row justify-between items-center px-6">
                         <div className="flex flex-col flex-1">
                             <h1 className="text-3xl">Lemon Template</h1>
