1fccfb3bfe0dd8aa5974a01171a6ed155bac5103
Andres6936 <andres6936@gmail.com>
2024-05-28 20:47:19 -0500

    Update responsive layouts in project components
    Adjusted the responsive layouts in ContainerProject, ContainerProjectMobil, and ContainerProjectWeb components. The order and flex properties have been updated for larger screen sizes (lg and upwards). This change improves the visual presentation and user interaction on larger screens.

--- src/components/ContainerProject.tsx
+++ src/components/ContainerProject.tsx
@@ -3,7 +3,7 @@ import {ReactNode} from "react";
 export default function ContainerProject({children}: { children: ReactNode }) {
     return (
         <section
-            className="flex flex-col md:flex-row gap-6 items-center justify-center md:w-5/6 lg:w-11/12 mx-auto mt-6">
+            className="flex flex-col lg:flex-row gap-6 items-center justify-center md:w-5/6 lg:w-11/12 mx-auto mt-6">
             {children}
         </section>
     )
