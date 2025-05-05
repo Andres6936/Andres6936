d5423d05450a6d26a183137d5ed55dbd1fa0e2ef
Andres6936 <andres6936@gmail.com>
2025-05-05 16:13:31 -0500

    Refactor ContactForm to use modular CSS for styling
    Moved ContactForm styles into a dedicated CSS module for better isolation and reusability. Simplified and removed redundant class names from global styles, improving maintainability and aligning with a scoped CSS approach.

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,5 +1,7 @@
 import {useState} from "react";
 
+import Styles from './ContactForm.module.css'
+
 const MAX_LIMIT_CHARACTERS = 255;
 
 export function ContactForm() {
@@ -8,7 +10,7 @@ export function ContactForm() {
     const [message, setMessage] = useState<string>('')
 
     return (
-        <form className="seva-form formkit-form cta cta--short cta__form" method="post"
+        <form className={Styles.Container} method="post"
               data-uid="c1cef76ca5" data-format="inline" data-version="5"
               data-options='{"settings":{"after_subscribe":{"action":"redirect","redirect_url":"https://www.kevinpowell.co/newsletter-success/","success_message":"Success! Now check your email to confirm your subscription."},"return_visitor":{"action":"show","custom_content":""},"recaptcha":{"enabled":false}}}'
               min-width="400 500 600 700 800">
@@ -27,38 +29,33 @@ export function ContactForm() {
 
                 <ul className="formkit-alert formkit-alert-error" data-element="errors" data-group="alert"></ul>
 
-                <div data-element="fields" data-stacked="true"
-                     className="seva-fields formkit-fields cta__grid gap:1rem">
-                    <div className="formkit-field">
-                        <input className="formkit-input" aria-label="Nombre" name="names"
-                               placeholder="Nombre" type="text"
-                               value={names}
-                               onChange={(e) => setNames(e.target.value)}
-                        />
-                    </div>
-                    <div className="formkit-field">
-                        <input className="formkit-input" aria-label="Correo" name="emails"
-                               placeholder="Correo" required type="email"
-                               value={email}
-                               onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </div>
+                <div data-element="fields" data-stacked="true" className={"gap:1rem " + Styles.ContainerGrid}>
+                    <input aria-label="Nombre" name="names"
+                           placeholder="Nombre" type="text"
+                           value={names}
+                           onChange={(e) => setNames(e.target.value)}
+                    />
+                    <input aria-label="Correo" name="emails"
+                           placeholder="Correo" required type="email"
+                           value={email}
+                           onChange={(e) => setEmail(e.target.value)}
+                    />
                 </div>
                 <div data-element="fields" data-stacked="true"
-                     className="seva-fields formkit-fields cta__grid gap:1rem gap-y:0rem">
-                    <div className="formkit-field" style={{gridColumn: "span 2"}}>
-                        <textarea className="formkit-textarea" aria-label="Mensaje" name="message"
+                     className={"gap:1rem gap-y:0rem " + Styles.ContainerGrid}>
+                    <div style={{gridColumn: "span 2"}}>
+                        <textarea className={Styles.TextAreaInput} aria-label="Mensaje" name="message"
                                   placeholder="Mensaje" rows={3} maxLength={MAX_LIMIT_CHARACTERS} required
                                   value={message}
                                   onChange={(e) => setMessage(e.target.value)}
                         />
                     </div>
-                    <p style={{gridColumn: "span 2"}} className="textarea-indicator-length">
+                    <p style={{gridColumn: "span 2"}} className={Styles.TextAreaIndicatorLength}>
                         Máx. {MAX_LIMIT_CHARACTERS - message.length} caracteres
                     </p>
                 </div>
 
-                <button data-element="submit" className="formkit-submit formkit-submit button button__cta">
+                <button data-element="submit" className="button">
                     <div className="formkit-spinner">
                         <div></div>
                         <div></div>
