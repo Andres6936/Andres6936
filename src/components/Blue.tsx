e2ea434a7ab706b4f5192b838b7c08449e294abd
Andres6936 <andres6936@gmail.com>
2025-05-11 12:33:34 -0500

    Refactor Blue component props formatting for readability
    Improved the formatting of props in the Blue component by spreading them across multiple lines. This enhances code readability and makes future modifications easier to manage. No functional changes were made.

--- src/components/Blue.tsx
+++ src/components/Blue.tsx
@@ -2,8 +2,14 @@ import {HomeSection} from "./HomeSection.tsx";
 
 export function Blue() {
     return (
-        <HomeSection to="/about" id="article-title" title="Sobre mi" subtitle="hitos, carrera & estudios"
-                     titleButton="¿quién soy?" variant="Article">
+        <HomeSection
+            to="/about"
+            id="article-title"
+            title="Sobre mi"
+            subtitle="hitos, carrera & estudios"
+            titleButton="¿quién soy?"
+            variant="Article"
+        >
             <p>
                 ¿Te interesa conocer el camino que me ha llevado hasta aquí? Te invito a
                 explorar los <strong>hitos clave de mi carrera</strong>, desde <strong>mis estudios </strong>
