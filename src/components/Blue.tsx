5d78490f0f0929a2dc03a92010af9c6b223c7c97
Andres6936 <andres6936@gmail.com>
2025-05-11 12:31:17 -0500

    Add reusable HomeSection component and refactor Blue.tsx
    Introduce a new `HomeSection` component to encapsulate section layout and styling logic. Refactor `Blue.tsx` to use `HomeSection`, improving code reusability and maintainability.

--- src/components/Blue.tsx
+++ src/components/Blue.tsx
@@ -1,22 +1,18 @@
+import {HomeSection} from "./HomeSection.tsx";
+
 export function Blue() {
     return (
-        <section aria-labelledby="article-title" className="home-section article">
-            <div className="container flow">
-                <header>
-                    <h2 id="article-title" className="section__title">Sobre mi</h2>
-                    <p className="section__subtitle">hitos, carrera & estudios</p>
-                </header>
-                <p>
-                    ¿Te interesa conocer el camino que me ha llevado hasta aquí? Te invito a
-                    explorar los <strong>hitos clave de mi carrera</strong>, desde <strong>mis estudios </strong>
-                    iniciales hasta mi experiencia profesional actual.
-                </p>
-                <p>
-                    Descubre las áreas en las que me he especializado y cómo mi formación ha moldeado mi enfoque y
-                    habilidades.
-                </p>
-                <a href="/articles" className="button">¿quién soy?</a>
-            </div>
-        </section>
+        <HomeSection to="/about" id="article-title" title="Sobre mi" subtitle="hitos, carrera & estudios"
+                     titleButton="¿quién soy?" variant="Article">
+            <p>
+                ¿Te interesa conocer el camino que me ha llevado hasta aquí? Te invito a
+                explorar los <strong>hitos clave de mi carrera</strong>, desde <strong>mis estudios </strong>
+                iniciales hasta mi experiencia profesional actual.
+            </p>
+            <p>
+                Descubre las áreas en las que me he especializado y cómo mi formación ha moldeado mi enfoque y
+                habilidades.
+            </p>
+        </HomeSection>
     )
 }
---
+++ src/components/HomeSection.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+type Variant = "Article"
+
+type Props = {
+    to: string,
+    id: string,
+    title: string,
+    subtitle: string,
+    titleButton: string,
+    variant: Variant,
+    children: React.ReactNode,
+}
+
+const getClassNameSection = (variant: Variant) => {
+    switch (variant) {
+        case "Article":
+            return "article"
+        default:
+            return ""
+    }
+}
+
+export function HomeSection(props: Props) {
+    return (
+        <section aria-labelledby={props.id} className={"home-section " + getClassNameSection(props.variant)}>
+            <div className="container flow">
+                <header>
+                    <h2 id={props.id} className="section__title">{props.title}</h2>
+                    <p className="section__subtitle">{props.subtitle}</p>
+                </header>
+                {props.children}
+                <a href={props.to} className="button">{props.titleButton}</a>
+            </div>
+        </section>
+    )
+}
