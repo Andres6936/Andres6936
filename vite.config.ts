ccdc310d667e155e037b4bece891834aa7cb6be7
Andres6936 <andres6936@gmail.com>
2026-09-15 07:58:33 -0500

    Migrating to React Router Framework

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { reactRouter } from "@react-router/dev/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [reactRouter()],
+  resolve: {
+    tsconfigPaths: true,
+  },
 });
